33aaaba1476ee0c14667461d5dee7baf9658c612
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-11-26 16:30:57 -0600

    Updates

---
+++ precache-manifest.93de9b25fceb590ea0187ede1975184a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b524a6f4c0cc497c1ccdeb2407da67b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "811e3e14143d8df7b86c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "37619b806aa8487e2b15",
+    "url": "/static/js/2.a1925c77.chunk.js"
+  },
+  {
+    "revision": "811e3e14143d8df7b86c",
+    "url": "/static/js/main.1c39a4d7.chunk.js"
+  },
+  {
+    "revision": "c5947460985e70391086",
+    "url": "/static/js/runtime-main.2e08c6c2.js"
+  }
+]);
